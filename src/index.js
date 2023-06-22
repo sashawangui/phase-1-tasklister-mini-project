69c28180bad695dd58629dd6c188dc86f62ec231
sasha wangui <sasha.iluku@gmail.com>
2023-06-22 22:19:39 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let content = document.getElementById("new-task-description")
+  let submit = document.getElementById('submit')
+  let list = document.getElementById('tasks')
+  
+  submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    let li = document.createElement('li');
+    li.textContent = `${content.value} `
+    let btn = document.createElement('button');
+    btn.textContent = "x";
+    btn.addEventListener('click', handleDelete);
+    li.append(btn)
+    list.append(li)
+    content.value = " "
+
+  })
+
+  
+  function handleDelete(e){
+    e.target.parentNode.remove();
+  } 
+
 });
+
+
+
+
+
